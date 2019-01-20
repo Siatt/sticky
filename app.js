6b96b8004d2a9cd4319c30c01fe80feb48b642e6
Lucas <luclucsjoqvist@gmail.com>
2019-01-20 18:54:30 +0100

    added structure and entry route

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views/pages/'));
+app.set('view engine', 'ejs');
+// Home Route
+app.get('/', (req,res) => res.render('index'));
+// Start Server
+app.listen(port, () => console.log(`App listening on port ${port}`));
